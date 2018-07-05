7185e027e9324bd546edb01d7e38e1e9ca5cb4e9
Vaslo <vaslo77@Gmail.com>
2018-07-04 22:23:01 -0400

    Continuing into chapter 3, created interpolate.js as instructed

--- shapes.js
+++ shapes.js
@@ -42,5 +42,55 @@ svg.selectAll("line")
 						.attr("stroke-width","2")
 						.attr("y1",function(d,i){return 80+(i*20);})
 						.attr("x2",function(d){return newX+(d*15);})
-						.attr("y2",function(d,i){return 80+(i+20);})
-	
+
+
+						.attr("y2",function(d,i){return 80+(i*20);});
+
+var textArray = ['start','middle','end'];
+
+svg.append("text").selectAll("tspan")
+	.data(textArray)
+	.enter().append("tspan")
+		.attr("x",newX)
+		.attr("y",function(d,i){return 150 +(i*30);})
+		.attr("fill","none")
+		.attr("stroke","blue")
+		.attr("stroke-width","2")
+		.attr("dominant-baseline","middle")
+		.attr("text-anchor","start")
+		.attr("font-size","30")
+		.text(function(d){return d;});
+
+// svg.append("text")
+// 			.attr("x",newX)
+// 			.attr("y",150)
+// 			.attr("fill","none")
+// 			.attr("stroke","blue")
+// 			.attr("stroke-width","2")
+// 			.attr("text-anchor","start")
+// 			.attr("dominant-baseline","middle")
+// 			.attr("font-size","30")
+// 			.text("start");
+		
+// svg.append("text")
+// 			.attr("x",newX)
+// 			.attr("y",180)
+// 			.attr("fill","blue")
+// 			.attr("stroke","none")
+// 			.attr("text-anchor","middle")
+// 			.attr("dominant-baseline","middle")
+// 			.attr("font-size","30")
+// 			.text("middle");
+
+// svg.append("text")
+// 			.attr("x",newX)
+// 			.attr("y",210)
+// 			.attr("fill","none")
+// 			.attr("stroke","blue")
+// 			.attr("text-anchor","middle")
+// 			.attr("dominant-baseline","middle")
+// 			.attr("font-size","30")
+// 			.text("end");
+
+
+			
